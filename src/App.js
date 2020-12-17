cc4b04c1ef3156c2691c505755f3ecc8d19741cb
J Grant Forrest <johngrant4est@users.noreply.github.com>
2020-12-17 10:24:56 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import NavigationBar from './components/NavigationBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from 'styled-components';
+import FunctionClick from './components/FunctionClick';
+
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: white;
+`;
+const Title = styled.h5`
+  padding-left: 8.5em;
+`;
 
 function App() {
-  return (
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <NavigationBar />     
+     </header> 
+    <Title>Interopen Care Plan Hackathon 16-17 Dec 2020</Title>  
+      <Wrapper>
+         
+        <Container>
+          <Row>
+            <Col>
+              <FunctionClick/>
+            </Col>
+          </Row>
+          <Row >
+            <Col lg={2}>
+              
+            </Col>
+            <Col lg={10}><div className="p-3 border bg-light" id="query_results">Results here</div></Col>
+          </Row>
+        </Container>             
+      </Wrapper>
+ 
+  </div>
+   );
 }
 
 export default App;
